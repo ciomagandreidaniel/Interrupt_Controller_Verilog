41de95ee1ac0fb9e19c6c815ca36fecb6225bbcd
ciomagandreidaniel <ciomagandreidaniel@gmail.com>
2020-07-29 18:24:12 +0300

    daisy chain update 1

--- testing/hdl under test/dcpirq.v
+++ testing/hdl under test/dcpirq.v
@@ -3,7 +3,7 @@ input clk,
 input rst_n,
 input enable,
 input [3:0] irq_trigger,
-output      int_output,
+output reg  int_output,
 output reg [1:0] irq_address
 );
 
@@ -12,10 +12,10 @@ output reg [1:0] irq_address
 // Priority
 //--------------------------------------------------------------
 
-reg [4:0] irq_reg_0;
-reg [4:0] irq_reg_1;
-reg [4:0] irq_reg_2;
-reg [4:0] irq_reg_3;
+reg [2:0] irq_reg_0;
+reg [2:0] irq_reg_1;
+reg [2:0] irq_reg_2;
+reg [2:0] irq_reg_3;
 
 wire        ack_irq;
 wire       irq_line;
@@ -32,16 +32,16 @@ always @(posedge clk or negedge rst_n)
 begin 
 if(~rst_n)
  begin
-  irq_reg_0 <= 5'b00000;
+  irq_reg_0 <= 3'b000;
  end
 else if(enable)
  begin
    if(irq_trigger[0])
     irq_reg_0[0] <= 1'b1;
    if(ack_irq) 
-    irq_reg_0[3] <= 1'b1;
-   if(irq_reg_0[3] & (~irq_reg_0[0]))
-    irq_reg_0[4] <= 1'b1;
+    irq_reg_0[1] <= 1'b1;
+   if(irq_reg_0[1] & (~irq_reg_0[0]))
+    irq_reg_0[2] <= 1'b1;
  end
 end
 
@@ -51,16 +51,16 @@ always @(posedge clk or negedge rst_n)
 begin 
 if(~rst_n)
  begin
-  irq_reg_1 <= 5'b00100;
+  irq_reg_1 <= 3'b000;
  end
 else if(enable)
  begin
    if(irq_trigger[1])
     irq_reg_1[0] <= 1'b1;
-   if(irq_reg_0[4]) 
-    irq_reg_1[3] <= 1'b1;
-   if(irq_reg_1[3] & (~irq_reg_1[0]))
-    irq_reg_1[4] <= 1'b1;	
+   if(irq_reg_0[2]) 
+    irq_reg_1[1] <= 1'b1;
+   if(irq_reg_1[1] & (~irq_reg_1[0]))
+    irq_reg_1[2] <= 1'b1;	
  end
 end
 
@@ -70,16 +70,16 @@ always @(posedge clk or negedge rst_n)
 begin 
 if(~rst_n)
  begin
-  irq_reg_2 <= 5'b00010;
+  irq_reg_2 <= 3'b000;
  end
 else if(enable)
  begin
    if(irq_trigger[2])
     irq_reg_2[0] <= 1'b1;
-   if(irq_reg_1[4]) 
-    irq_reg_2[3] <= 1'b1;
-   if(irq_reg_2[3] & (~irq_reg_2[0]))
-    irq_reg_2[4] <= 1'b1;
+   if(irq_reg_1[2]) 
+    irq_reg_2[1] <= 1'b1;
+   if(irq_reg_2[1] & (~irq_reg_2[0]))
+    irq_reg_2[2] <= 1'b1;
  end
 end
 
@@ -90,16 +90,16 @@ always @(posedge clk or negedge rst_n)
 begin 
 if(~rst_n)
  begin
-  irq_reg_3 <= 5'b00110;
+  irq_reg_3 <= 3'b000;
  end
 else if(enable)
  begin
    if(irq_trigger[3])
     irq_reg_3[0] <= 1'b1;
-   if(irq_reg_2[4]) 
-    irq_reg_3[3] <= 1'b1;
-   if(irq_reg_3[3] & (~irq_reg_3[0]))
-    irq_reg_3[4] <= 1'b1;
+   if(irq_reg_2[2]) 
+    irq_reg_3[1] <= 1'b1;
+   if(irq_reg_3[1] & (~irq_reg_3[0]))
+    irq_reg_3[2] <= 1'b1;
  end
 end
 
@@ -108,13 +108,19 @@ assign irq_line = irq_reg_0[0] | irq_reg_1[0] | irq_reg_2[0] | irq_reg_3[0];
 
 assign ack_irq = irq_line;
 
-assign irq_out_0 = irq_reg_0[0] &  irq_reg_0[3];
-assign irq_out_1 = irq_reg_1[0] &  irq_reg_1[3];
-assign irq_out_2 = irq_reg_2[0] &  irq_reg_2[3];
-assign irq_out_3 = irq_reg_3[0] &  irq_reg_3[3];
+assign irq_out_0 = irq_reg_0[0] &  irq_reg_0[1];
+assign irq_out_1 = irq_reg_1[0] &  irq_reg_1[1];
+assign irq_out_2 = irq_reg_2[0] &  irq_reg_2[1];
+assign irq_out_3 = irq_reg_3[0] &  irq_reg_3[1];
 
-assign int_output =  irq_out_0 | irq_out_1 | irq_out_2 | irq_out_3; 
 					
+always @(posedge clk or negedge rst_n)
+begin
+ if(~rst_n)
+   int_output = 1'b0;
+ else if(enable)
+   int_output <=  irq_out_0 | irq_out_1 | irq_out_2 | irq_out_3;
+end 
 
 //irq_address
 always @(posedge clk or negedge rst_n)
@@ -122,13 +128,13 @@ begin
  if(~rst_n) irq_address <= 2'b00;
  else if (enable)
   begin
-   if(irq_reg_0[3] & irq_reg_0[0])
+   if(irq_reg_0[1] & irq_reg_0[0])
        irq_address <= 2'b00;
-   else if(irq_reg_1[3] & irq_reg_1[0])
+   else if(irq_reg_1[1] & irq_reg_1[0])
        irq_address <= 2'b01;
-   else if(irq_reg_2[3] & irq_reg_2[0])
+   else if(irq_reg_2[1] & irq_reg_2[0])
        irq_address <= 2'b10;
-   else if(irq_reg_3[3] & irq_reg_3[0])
+   else if(irq_reg_3[1] & irq_reg_3[0])
        irq_address <= 2'b11;
   end
 end
